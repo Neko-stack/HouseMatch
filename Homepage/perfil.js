b936f0cfcfd98a14add46a4fada54b8f5b943ddc
GABRIEL LESSA COELHO <gabriel_l_coelho@estudante.sesisenai.org.br>
2025-06-23 22:05:55 -0300

    add script localStorage

--- Homepage/perfil.js
+++
@@ -1,7 +0,0 @@
-function deslogar(){
-    document.getElementById("container-home").style.display = "flex"
-    document.getElementById("open-login").style.display = "flex"
-    document.getElementById("dashboard").style.display = "none"
-    document.getElementById("configuracoes").style.display = "none"
-    document.getElementById("container-perfil").style.display = "none"
-}
